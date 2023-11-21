a31bc2ba25bc8460bc69d278b237c1fc2fca6ccc
saydinaambiya <ssyyddn@gmail.com>
2023-11-21 20:39:09 +0700

    Initial commit

---
+++ src/main/java/com/amazon/yudoo/YudooApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.yudoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YudooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YudooApplication.class, args);
+	}
+
+}
